f076796b60438ae327beb70a95d8042275fdb269
Mohammad Al Jadallah <70474875+mohammadjadallah@users.noreply.github.com>
2022-05-20 14:36:25 +0300

    This will annoy your friends

---
+++ prankFriends.py
@@ -0,0 +1,13 @@
+# Pranks your friends using tkinter library
+
+from tkinter import messagebox
+from tkinter import Tk
+
+Tk().withdraw()  # to get rid of the root screen
+
+
+while 1:
+
+    messagebox.showerror("Hacked", "You Are Hacked")  # title= , message=
+    # Finished
+    
